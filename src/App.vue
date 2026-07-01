4261b7c950312a147c64ede3b1ed59129c1c3d45
Openlist Developer <developer@openlist.com>
2026-07-01 18:25:58 +0000

    fix: replace unicode fullscreen symbols with responsive SVG vector icons

--- src/App.vue
+++ src/App.vue
@@ -288,8 +288,12 @@ onUnmounted(() => {
       </div>
       <div class="header-actions">
         <button class="fullscreen-btn" @click="toggleFullscreen" :title="isFullscreen ? 'Exit Fullscreen' : 'Enter Fullscreen'">
-          <span v-if="isFullscreen">🗗</span>
-          <span v-else>🗖</span>
+          <svg v-if="isFullscreen" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M4 14h6v6m10-6h-6v6M4 10h6V4m10 6h-6V4" />
+          </svg>
+          <svg v-else xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3" />
+          </svg>
         </button>
         <div class="badge" :class="{ 'paused': isPaused }">
           {{ isPaused ? 'PAUSED' : 'ACTIVE' }}
